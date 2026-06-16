831c80c1b2796c82c3cf1490640808d2ca8aa374
Akos Becsey <becsey.a@gmail.com>
2026-06-16 06:59:50 +0200

    Scraper + map

---
+++ scraper/aggregate.py
@@ -0,0 +1,49 @@
+from bs4 import BeautifulSoup
+import json
+import sys
+
+soup = BeautifulSoup(sys.stdin.read(), 'html.parser')
+tables = soup.find_all('table')
+table_with_data = tables[1]
+
+places = {}
+
+for event in table_with_data.find_all('tr'):
+    fields = event.find_all('td')
+    if not fields:
+        continue
+
+    name = fields[3].text.strip()
+    address = fields[4].text.strip()
+    start = fields[1].text.strip()
+
+    book_link = fields[0].find('a')
+    if book_link:
+        date = book_link.text.strip()
+        end = fields[2].find_all(string=True, recursive=False)[0].strip()
+        booking_id = book_link['href'].split('ssid=')[1]
+    else:
+        date = fields[0].text.strip()
+        end = fields[2].text.strip()
+        booking_id = None
+
+    id = hash(address)
+    appointment = {
+        "date": date,
+        "start": start,
+        "end": end,
+        "booking_id": booking_id
+    }
+
+    if id in places:
+        places[id]['appointments'].append(appointment)
+    else:
+        places[id] = ({
+            'name': name,
+            'address': address,
+            'appointments': [appointment],
+        })
+
+places = list(places.values())
+
+print(json.dumps(places, indent=4, ensure_ascii=False))
---
+++ scraper/decorate.py
@@ -0,0 +1,83 @@
+import json
+import sys
+import os
+import requests
+
+ADDRESS_CACHE_FILE = 'address_cache.json'
+API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
+
+
+def load_cache():
+    try:
+        with open(ADDRESS_CACHE_FILE, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def is_fixed_location(place):
+    nameLower = place['name'].lower()
+    KEYWORDS = [
+        'vérellátó', 'vérell.',
+        'transzfúziós', 'transzf.',
+        'forráspont pécs',
+        'sugár üzletközpont'
+    ]
+    return nameLower.endswith("intézeti") or any(keyword in nameLower for keyword in KEYWORDS)
+
+
+def call_geocode_api(address):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address=Hungary {address}&key={API_KEY}"
+    return requests.get(url).json()
+
+
+def call_geocode_api_best_effort(address):
+    # tolerate network errors, flaky API responses, unknown addresses
+
+    ATTEMPTS = 5
+    dropped_street = False
+    for _ in range(ATTEMPTS):
+        try:
+            data = call_geocode_api(address)
+
+            if data['status'] == 'OK':
+                return data
+            elif data['status'] == 'ZERO_RESULTS' and not dropped_street:
+                address = address.split(',')[0]  # drop street
+                dropped_street = True
+
+        except Exception as _:
+            pass
+
+    raise Exception(f"Failed to get coordinates for address: {address}")
+
+
+def get_coordinates_cached(address, cache):
+    if address in cache:
+        return cache[address]
+
+    sys.stderr.write(f"Fetching {address}...\n")
+
+    data = call_geocode_api_best_effort(address)
+
+    l = data['results'][0]['geometry']['location']
+    coords = [l['lat'], l['lng']]
+
+    cache[address] = coords
+    return coords
+
+
+if __name__ == "__main__":
+
+    places = json.load(sys.stdin)
+    addresses = load_cache()
+
+    for place in places:
+        place['is_fixed_location'] = is_fixed_location(place)
+        place['coords'] = get_coordinates_cached(place['address'], addresses)
+
+    # save
+    with open(ADDRESS_CACHE_FILE, 'w', encoding='utf-8') as f:
+        json.dump(addresses, f, indent=4, ensure_ascii=False)
+
+    json.dump(places, sys.stdout, indent=4, ensure_ascii=False)
