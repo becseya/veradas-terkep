4e87dff47c725fc4f6a12d9be7034053c8924c2f
Akos Becsey <becsey.a@gmail.com>
2026-06-29 20:18:34 +0200

    Change detection

--- scraper/decorate.py
+++ scraper/decorate.py
@@ -3,14 +3,24 @@ import sys
 import os
 import requests
 from urllib.parse import quote
+from datetime import datetime
 
 ADDRESS_CACHE_FILE = 'address_cache.json'
+LOCATION_HISTORY_FILE = 'location_history.json'
 API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
 
 
 def load_cache():
     try:
         with open(ADDRESS_CACHE_FILE, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+
+def load_history():
+    try:
+        with open(LOCATION_HISTORY_FILE, 'r', encoding='utf-8') as f:
             return json.load(f)
     except FileNotFoundError:
         return {}
@@ -98,8 +108,52 @@ def get_intervals(start: str, end: str):
     return result
 
 
+def get_place_id(place):
+    # We are using coordinates as a UID to tolerate minor changes in address / name.
+    return '_'.join(map(str, place['coords']))
+
+
+def compare_appointment_lists(current, old):
+    # TODO: Detect if future appointment is being removed
+    # TODO: Fading past appointments are not a real change
+    # TODO: Might contain 'NEW' appointment, even if already present in history
+    if set(current) == set(old):
+        return 'SAME'
+    else:
+        return 'CHANGED'
+
+
+def update_history(history, place):
+    id = get_place_id(place)
+    appointments = list(place['appointments'].keys())
+    now = datetime.now().isoformat()
+
+    if id not in history:
+        history[id] = {
+            'appointments': appointments,
+            'lastModified': now,
+        }
+        state = 'NEW'
+    else:
+        old_appointments = history[id]['appointments']
+        state = compare_appointment_lists(appointments, old_appointments)
+
+        if state != 'SAME':
+            history[id]['appointments'] = appointments
+            history[id]['lastModified'] = now
+
+    return state
+
+
+def cull_history(history, places):
+    current_ids = {get_place_id(place) for place in places}
+
+    for id in list(history.keys()):
+        if id not in current_ids:
+            del history[id]
+
+
 def get_day_of_week(date_str: str) -> int:
-    from datetime import datetime
     date_obj = datetime.strptime(date_str, "%Y-%m-%d")
     return date_obj.weekday()  # Monday is 0 and Sunday is 6
 
@@ -108,6 +162,7 @@ if __name__ == "__main__":
 
     places = json.load(sys.stdin)
     addresses = load_cache()
+    history = load_history()
 
     for place in places:
         place['is_fixed_location'] = is_fixed_location(place)
@@ -117,8 +172,16 @@ if __name__ == "__main__":
             apt['intervals'] = get_intervals(apt['start'], apt['end'])
             apt['dayOfWeek'] = get_day_of_week(date)
 
+        place['state'] = update_history(history, place)
+        place['lastModified'] = history[get_place_id(place)]['lastModified']
+
+    cull_history(history, places)
+
     # save
     with open(ADDRESS_CACHE_FILE, 'w', encoding='utf-8') as f:
         json.dump(addresses, f, indent=4, ensure_ascii=False)
+
+    with open(LOCATION_HISTORY_FILE, 'w', encoding='utf-8') as f:
+        json.dump(history, f, indent=4, ensure_ascii=False)
 
     json.dump(places, sys.stdout, indent=4, ensure_ascii=False)
