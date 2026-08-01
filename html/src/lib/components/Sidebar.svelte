ccaf879f17883e435452f75e4f22985bc405cd39
Akos Becsey <becsey.a@gmail.com>
2026-08-01 09:59:52 +0200

    Collapse filter by default on mobile

--- html/src/lib/components/Sidebar.svelte
+++ html/src/lib/components/Sidebar.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onMount } from 'svelte';
   import FilterPanel from './FilterPanel.svelte';
   import InspectorPanel from './InspectorPanel.svelte';
   import SubscribePanel from './SubscribePanel.svelte';
@@ -7,10 +8,22 @@
   export let filters;
 
   let filtersCollapsed = false;
+  let isMobile = false;
 
-  $: if (selectedLocation) {
-    filtersCollapsed = true;
-  }
+  onMount(() => {
+    const mediaQuery = window.matchMedia('(max-width: 768px)');
+
+    const syncViewportState = () => {
+      isMobile = mediaQuery.matches;
+    };
+
+    syncViewportState();
+    mediaQuery.addEventListener('change', syncViewportState);
+
+    return () => mediaQuery.removeEventListener('change', syncViewportState);
+  });
+
+  $: filtersCollapsed = selectedLocation ? true : isMobile;
 </script>
 
 <aside class="sidebar-shell">
